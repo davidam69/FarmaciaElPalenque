336b3bc3dc7abf2fb6d5c526727323e2d157ad53
joaquin comello <joaquincomello@gmail.com>
2025-07-06 18:57:18 -0300

    integracion de la ia con sql estoy teniendo problemas para probarlo

--- FarmaciaElPalenque/MlModel/ClienteClustering.cs
+++ FarmaciaElPalenque/MlModel/ClienteClustering.cs
@@ -1,24 +1,53 @@
 ﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
 
 namespace FarmaciaElPalenque.MlModel
 {
     public class ClienteClustering
     {
-
-        private readonly string rutaCSV;
+        private readonly string connectionString;
         private readonly MLContext mlContext;
 
-        public ClienteClustering(string rutaCSV)
+        public ClienteClustering(string connectionString)
         {
-            this.rutaCSV = rutaCSV;
+            this.connectionString = connectionString;
             this.mlContext = new MLContext();
         }
 
         public List<(ClienteData Cliente, uint Cluster, bool DeberiaAvisarse)> EjecutarClustering()
         {
-            IDataView dataView = mlContext.Data.LoadFromTextFile<ClienteData>(
-                rutaCSV, separatorChar: ',', hasHeader: true);
+            // 1. Leer datos desde la base
+            var clientes = new List<ClienteData>();
 
+            using (var conexion = new SqlConnection(connectionString))
+            {
+                conexion.Open();
+                var query = "SELECT Edad, ComprasMensuales, MontoGastado, DiasDesdeUltimaCompra FROM Clientes";
+
+                using (var comando = new SqlCommand(query, conexion))
+                using (var lector = comando.ExecuteReader())
+                {
+                    while (lector.Read())
+                    {
+                        clientes.Add(new ClienteData
+                        {
+                            Edad = Convert.ToSingle(lector["Edad"]),
+                            ComprasMensuales = Convert.ToSingle(lector["ComprasMensuales"]),
+                            MontoGastado = Convert.ToSingle(lector["MontoGastado"]),
+                            DiasDesdeUltimaCompra = Convert.ToSingle(lector["DiasDesdeUltimaCompra"])
+                        });
+                    }
+                }
+            }
+
+            // 2. Cargar datos en ML.NET
+            var dataView = mlContext.Data.LoadFromEnumerable(clientes);
+
+            // 3. Definir pipeline de clustering
             var pipeline = mlContext.Transforms.Concatenate("Features",
                     nameof(ClienteData.Edad),
                     nameof(ClienteData.ComprasMensuales),
@@ -27,23 +56,25 @@ namespace FarmaciaElPalenque.MlModel
                 .Append(mlContext.Transforms.NormalizeMinMax("Features"))
                 .Append(mlContext.Clustering.Trainers.KMeans("Features", numberOfClusters: 3));
 
+            // 4. Entrenar el modelo
             var modelo = pipeline.Fit(dataView);
             var predicciones = modelo.Transform(dataView);
 
+            // 5. Obtener resultados de clustering
             var resultados = mlContext.Data
                 .CreateEnumerable<PrediccionCliente>(predicciones, reuseRowObject: false)
                 .ToList();
 
-            var datosOriginales = mlContext.Data
-                .CreateEnumerable<ClienteData>(dataView, reuseRowObject: false)
-                .ToList();
-
             var hoy = DateTime.Today;
 
-            var combinados = datosOriginales.Zip(resultados, (cliente, prediccion) =>
+            // 6. Combinar con la lógica de aviso personalizada
+            var combinados = clientes.Zip(resultados, (cliente, prediccion) =>
             {
+                // Simula que la última compra fue hace X días
                 var fechaUltimaCompra = hoy.AddDays(-cliente.DiasDesdeUltimaCompra);
                 var diasDesdeUltimaCompra = (hoy - fechaUltimaCompra).TotalDays;
+
+                // Si se pasó por más de 2 días de su frecuencia habitual, avisar
                 bool debeAvisarse = diasDesdeUltimaCompra > cliente.DiasDesdeUltimaCompra + 2;
 
                 return (cliente, prediccion.PredictedClusterId, debeAvisarse);
@@ -55,24 +86,22 @@ namespace FarmaciaElPalenque.MlModel
         public void AnalizarDiasDeCompra(List<(ClienteData Cliente, uint Cluster)> resultados)
         {
             var agrupados = resultados
-                .Select(r => new CompraConFecha { Cliente = r.Cliente, Cluster = r.Cluster })
-                .GroupBy(cf => cf.Cluster)
-                .ToList();
+                .Select(r => new { r.Cliente, r.Cluster })
+                .GroupBy(x => x.Cluster);
 
             foreach (var grupo in agrupados)
             {
                 Console.WriteLine($"Cluster {grupo.Key}:");
 
-                var dias = grupo.Select(x => (int)x.DiasDesdeUltimaCompra)
-                                .GroupBy(d => d)
-                                .OrderByDescending(g => g.Count());
+                var dias = grupo
+                    .Select(x => (int)x.Cliente.DiasDesdeUltimaCompra)
+                    .GroupBy(d => d)
+                    .OrderByDescending(g => g.Count());
 
                 foreach (var dia in dias)
                 {
-                    Console.WriteLine($"  Día {dia.Key}: {dia.Count()} compras");
+                    Console.WriteLine($"  Día {dia.Key}: {dia.Count()} clientes");
                 }
-
-
             }
         }
     }
