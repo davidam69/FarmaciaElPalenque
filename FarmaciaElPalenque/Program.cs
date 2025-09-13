55d1390ba77a7527ac412f9c156ebfde3f383ed3
david Miranda <david_m_pericon@hotmail.com>
2025-09-13 18:36:49 -0300

    cambios 3

--- FarmaciaElPalenque/Program.cs
+++ FarmaciaElPalenque/Program.cs
@@ -3,26 +3,26 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
+//para guardar en memoria 30 minutos
 builder.Services.AddSession(options =>
 {
-    options.Cookie.Name = ".FarmaciaElPalenque.Session";
-    options.IdleTimeout = TimeSpan.FromMinutes(60); // tiempo de inactividad
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // tiempo de inactividad permitido
     options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true; // necesario para que no lo bloquee el consentimiento de cookies
+    options.Cookie.IsEssential = true;
 });
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ConexionSQL")));
 
 var app = builder.Build();
-
-
-if (!app.Environment.IsDevelopment())
+/*
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
+    // GET: /dev/bcrypt?password=admin123
+    app.MapGet("/dev/bcrypt", (string password) =>
+        BCrypt.Net.BCrypt.HashPassword(password));
+}*/
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
