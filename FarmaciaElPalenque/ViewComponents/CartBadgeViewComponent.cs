536c20ab4bb193d02cd48f6a155c88ec0ba67d85
david Miranda <david_m_pericon@hotmail.com>
2025-10-07 01:49:48 -0300

    agregado de un carrito flotante

---
+++ FarmaciaElPalenque/ViewComponents/CartBadgeViewComponent.cs
@@ -0,0 +1,14 @@
+﻿namespace FarmaciaElPalenque.ViewComponents
+{
+    public class CartBadgeViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            // Usa tus extensiones de sesión
+            var cart = HttpContext.Session.ObtenerCarrito() ?? new List<Carrito>();
+            var count = cart.Sum(i => i.Cantidad);
+
+            return View(count); // pasa el número como modelo
+        }
+    }
+}
