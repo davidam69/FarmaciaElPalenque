7366b5e2c3522160779e44d2e23d3a977dc1e690
david Miranda <david_m_pericon@hotmail.com>
2025-10-07 17:25:49 -0300

    se quitaron los msjs de posible no null y los using se movieron al usings global

--- FarmaciaElPalenque/MlModel/ClienteClustering.cs
+++ FarmaciaElPalenque/MlModel/ClienteClustering.cs
@@ -1,11 +1,4 @@
-﻿using Microsoft.ML;
-using Microsoft.ML.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Data.SqlClient;
-
-namespace FarmaciaElPalenque.MlModel
+﻿namespace FarmaciaElPalenque.MlModel
 {
     public class ClienteClustering
     {
--- FarmaciaElPalenque/MlModel/ClienteData.cs
+++ FarmaciaElPalenque/MlModel/ClienteData.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.ML.Data;
-
-namespace FarmaciaElPalenque.MlModel
+﻿namespace FarmaciaElPalenque.MlModel
 {
     public class ClienteData
     {
